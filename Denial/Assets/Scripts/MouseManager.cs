8da81e1dae9bb29cc32052ca76acfc3a27cd6b04
Parker Johansen <johansen.parker@gmail.com>
2019-01-26 03:02:28 -0700

    Create basic room and cursor changes

---
+++ Denial/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    // Know what objects are clickable
+    public LayerMask clickableLayer;
+    
+    // Swap Cursors per object
+    public Texture2D pointer;
+    public Texture2D target;
+    public Texture2D doorway;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if (hit.collider.gameObject.tag.Equals(nameof(doorway)))
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16,16),CursorMode.Auto);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
