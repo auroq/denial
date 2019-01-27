5673d5084c70e17e26f4963a0553ee45ea206b0e
Parker Johansen <johansen.parker@gmail.com>
2019-01-27 00:54:17 -0700

    Implemented interaction with objects

---
+++ Denial/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/CrosshairManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairManager : MonoBehaviour
+{
+    [SerializeField] private PlayerInteractionsHelper PlayerInteractionsHelper;
+    
+    public LayerMask clickableLayer;
+
+        // Swap Cursors per object
+    public Sprite pointer;
+    public Sprite target;
+    public Sprite doorway;
+
+    private Image image;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerInteractionsHelper.Init();
+        image = GetComponent<Image>();
+        image.sprite = pointer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleCrosshair();
+        HandleInteraction();
+    }
+
+    void HandleCrosshair()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 2, clickableLayer.value))
+        {
+            if (hit.collider.gameObject.tag.Equals(nameof(Clickables.Doorway)))
+                image.sprite = doorway;
+            else
+                image.sprite = target;
+        }
+        else
+        {
+           image.sprite = pointer;
+        }
+    }
+
+    void HandleInteraction()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 2, clickableLayer.value)
+            && Input.GetMouseButton(0))
+        {
+            PlayerInteractionsHelper.HandleInteraction(hit.collider.gameObject);
+        }
+    }
+}
---
+++ Denial/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerInteractionsHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public enum Clickables
+{
+    Doorway = 0,
+    Lamp = 1,
+}
+
+[Serializable]
+public class PlayerInteractionsHelper
+{
+    public float interactionDebounce = .5f;
+    
+    private ConcurrentDictionary<int, DateTime> Interactions;
+
+    public void Init()
+    {
+        Interactions = new ConcurrentDictionary<int, DateTime>();
+    }
+
+    private bool DebounceComplete(int objectId)
+    {
+        var now = DateTime.UtcNow;
+        if (Interactions.ContainsKey(objectId) &&
+            (now - Interactions[objectId]).TotalSeconds < interactionDebounce)
+            return false;
+        
+        return true;
+    }
+
+    private void LogDebounce(int objectId)
+    {
+        var now = DateTime.UtcNow;
+        Interactions.AddOrUpdate(objectId, now, (id, date) => now);
+    }
+    
+    public void HandleInteraction(GameObject gameObject)
+    {
+        var id = gameObject.GetInstanceID();
+        if (DebounceComplete(id))
+        {
+            LogDebounce(id);
+            switch (gameObject.tag)
+            {
+                case nameof(Clickables.Doorway):
+                    HandleDoorway(gameObject);
+                    break;
+                case nameof(Clickables.Lamp):
+                    HandleLamp(gameObject);
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+
+    private void HandleDoorway(GameObject doorway)
+    {
+        
+    }
+
+    private void HandleLamp(GameObject lamp)
+    {
+        var light = lamp.GetComponentInChildren<Light>();
+        light.enabled = !light.enabled;
+    }
+}
